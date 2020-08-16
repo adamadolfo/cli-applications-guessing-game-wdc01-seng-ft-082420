70346bfa357be9f517fafe19e53b65ba58994556
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-16 16:41:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,6 @@ def run_guessing_game
   elsif guess == 'exit'
     puts "Goodbye!"
   else
-    "Sorry! The computer guessed #{random_number}."
+    puts "Sorry! The computer guessed #{random_number}."
   end
 end
