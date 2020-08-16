bddcaeb0eba63e651f335eff81710b3936719030
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-16 16:40:27 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  random_number = rand(1..6).to_s
+  random_number = 
   guess = gets.chomp
     if guess == random_number
     puts "You guessed the correct number!"
